360e35b58b1ae6637fe447438921d49b39fd5da5
John Vesey <jvesey@agl.com.au>
2017-12-05 09:15:39 +1100

    Sunday

--- api/Controllers/TriviaController.cs
+++ api/Controllers/TriviaController.cs
@@ -22,7 +22,7 @@ namespace api.Controllers
             var response = await httpClient.GetAsync("http://numbersapi.com/" + number + "?json");
             var triviaResult = await response.Content.ReadAsStringAsync();
             var res = JsonConvert.DeserializeObject<TriviaResponse>(triviaResult);
-            res.Text += " YES SIR EEE!";
+            res.Text += " GIVEN";
             return res;
         }
     }
