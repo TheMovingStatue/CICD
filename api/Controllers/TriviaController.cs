dedc8370b2d1bc871610ecf0cd393ee59d273433
John Vesey <jvesey@agl.com.au>
2017-12-04 22:18:59 +1100

    Agaon

--- api/Controllers/TriviaController.cs
+++ api/Controllers/TriviaController.cs
@@ -18,6 +18,7 @@ namespace api.Controllers
         public async Task<TriviaResponse> GetAsync(int number)
         {
             var x=10;
+            x=x+22;
             //Call numbersapi.com and return results
             var httpClient = new HttpClient();
             var response = await httpClient.GetAsync("http://numbersapi.com/" + number + "?json");
