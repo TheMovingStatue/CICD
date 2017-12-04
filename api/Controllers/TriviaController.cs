97ff2056a1d0895957a1d31ddbf8bbedfcfad083
John Vesey <jvesey@agl.com.au>
2017-12-05 08:44:48 +1100

    EEE

--- api/Controllers/TriviaController.cs
+++ api/Controllers/TriviaController.cs
@@ -22,7 +22,7 @@ namespace api.Controllers
             var response = await httpClient.GetAsync("http://numbersapi.com/" + number + "?json");
             var triviaResult = await response.Content.ReadAsStringAsync();
             var res = JsonConvert.DeserializeObject<TriviaResponse>(triviaResult);
-            res.Text += " YES SIR";
+            res.Text += " YES SIR EEE";
             return res;
         }
     }
